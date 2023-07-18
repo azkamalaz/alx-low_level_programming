9ee13b4c4455633faa7f16cc2f6945de994f0bbf
azkamalaz <kamalazenag@gmail.com>
2023-07-18 18:36:49 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
   * main - Entry point
   *
@@ -9,6 +8,14 @@
 */
 int main(void)
 {
-	putchar(_putchar);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
