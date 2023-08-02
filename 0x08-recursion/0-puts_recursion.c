61a38c8c52e53037b5d8ce09357d1f9115ad751d
azkamalaz <kamalazenag@gmail.com>
2023-08-02 20:56:53 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,21 @@
-iid
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - func to print a str followed by a newline character.
+ * The function prints each character of the input string one by one
+ * until the end of the string is reached, and then adds a newline character.
+ *
+ * @s: The input C-style string to be printed.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
