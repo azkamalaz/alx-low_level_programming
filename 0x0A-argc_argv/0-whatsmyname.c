05403e4bb0045f67aecd36442d3bff9944ff3489
azkamalaz <kamalazenag@gmail.com>
2023-08-04 21:13:35 +0000

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - main function of the program, which serves as the entry point.
+ * @argc: The number of command-line arguments passed to the program.
+ * @argv: An array of strings containing the command-line arguments.
+ * Return: Always 0.
+ */
 int main(int argc, char **argv)
 {
 	int i;
