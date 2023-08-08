3ecaad743b07e832cdd0b54e3afa2af75608a494
azkamalaz <kamalazenag@gmail.com>
2023-08-08 11:49:08 +0000

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strdup.c -o s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - dynamically allocating memory to create a copy of the input string
+ *
+ *@str: The source string to be duplicated.
+ *
+ * Return: A pointer to the dup string, or NULL if mem alloc fails.
+ */
+
+char *_strdup(char *str)
+{
+        size_t length = strlen(str);
+
+        char *duplicate = (char *)malloc(length + 1);
+
+        if (duplicate != NULL)
+        {
+                strcpy(duplicate, str);
+        }
+        return (duplicate);
+}
