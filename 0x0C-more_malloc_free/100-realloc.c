dd34eace00eed583d2a15c713098e755c505b927
azkamalaz <kamalazenag@gmail.com>
2023-08-10 22:42:46 +0000

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *ptr2;
 
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
