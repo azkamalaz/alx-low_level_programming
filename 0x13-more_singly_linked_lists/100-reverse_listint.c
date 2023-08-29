0ceb0cd11d9be66ba6555e607dfe31754a39b7b4
azkamalaz <kamalazenag@gmail.com>
2023-08-29 17:37:36 +0000

    update tasks

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,22 +1,27 @@
 #include "lists.h"
+
 /**
- * reverse_listint - Reverses a singly linked list of integers.
- * @head: A pointer to a pointer to the head of the linked list.
- * Return: A pointer to the new head of the reversed linked list.
+ * reverse_listint - the function that reverses a listint_t
+ * @head: double pointer
+ *
+ * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prv = NULL;
-	listint_t *next = NULL;
+	listint_t *previous, *next;
 
-	while (*head)
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	previous = NULL;
+	while (*head != NULL)
 	{
 		next = (*head)->next;
-		(*head)->next = prv;
-		prv = *head;
+		(*head)->next = previous;
+		previous = *head;
 		*head = next;
 	}
-	*head = prv;
-
+	*head = previous;
 	return (*head);
 }
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the head of the linked list.
- * Return: The number of nodes in the list.
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *current = head;
-	const listint_t *checker = head;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		current = current->next;
-
-		while (checker < current && checker != NULL)
+		for (i = 0; i < num; i++)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit(98);
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
 		}
-
-		if (checker != NULL)
-			checker = checker->next;
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
-	return (count);
+	free(list);
+	return (num);
 }
