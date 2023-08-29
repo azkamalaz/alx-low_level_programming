a197ea6ee4d82d3144233498a97481311e0f6247
azkamalaz <kamalazenag@gmail.com>
2023-08-29 01:43:58 +0000

    mandatory tasks update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2->n = n;
 	ptr2->next = NULL;
 
-	if (head != NULL)
+	if (!ptr2 || !head)
 	{
 		while (idx != 1)
 		{
