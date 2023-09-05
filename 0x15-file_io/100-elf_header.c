8dc651a2452dd1c1f7414bdd626a9561d0d265ca
azkamalaz <kamalazenag@gmail.com>
2023-09-05 01:13:12 +0000

    Update advanced task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,79 +1,54 @@
 #include "main.h"
 /**
- * create_buffer - Allocates a buffer for file operations.
- * @file: The name of the file for error messages.
- * Return: A pointer to the allocated buffer.
+ * main - Displays the information contained in the
+ * ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the file is not an ELF File or
+ * the function fails - exit code 98.
  */
-char *create_buffer2(const char *file)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char *buffer = malloc(sizeof(char) * 1024);
+	Elf64_Ehdr *header;
+	int o, r;
 
-	if (buffer == NULL)
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
 	{
-		fprintf(stderr, "Error: Can't allocate memory for %s\n", file);
-		exit(EXIT_FAILURE);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
 	}
 
-	return (buffer);
-}
-/**
- * close_file - Closes a file descriptor and handles errors.
- * @fd: The file descriptor to close.
- */
-void close_file(int fd)
-{
-	if (close(fd) == -1)
-	{
-		fprintf(stderr, "Error: Can't close file descriptor %d\n", fd);
-		exit(EXIT_FAILURE);
-	}
-}
-/**
- * main - Copies the contents of one file to another.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
- * Return: 0 on success, exit codes for various error conditions.
- */
-int main(int argc, char *argv[])
-{
-	int src_fd, dest_fd, bytes_read, bytes_written;
-	char *buffer;
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osabi(header->e_ident);
+	print_abi(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		exit(EXIT_FAILURE);
-	}
-	buffer = create_buffer2(argv[2]);
-	src_fd = open(argv[1], O_RDONLY);
-	if (src_fd == -1)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(EXIT_FAILURE);
-	}
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (dest_fd == -1)
-	{
-		fprintf(stderr, "Error: Can't create or write to %s\n", argv[2]);
-		free(buffer);
-		close_file(src_fd);
-		exit(EXIT_FAILURE);
-	}
-	while ((bytes_read = read(src_fd, buffer, 1024)) > 0)
-	{
-		bytes_written = write(dest_fd, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			close_file(src_fd);
-			close_file(dest_fd);
-			exit(EXIT_FAILURE);
-		}
-	}
-	free(buffer);
-	close_file(src_fd);
-	close_file(dest_fd);
+	free(header);
+	close_elf(o);
 	return (0);
 }
