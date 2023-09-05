a8a60797e4ff49e17434e3bb14c3406f1020390b
azkamalaz <kamalazenag@gmail.com>
2023-09-05 00:38:50 +0000

    Update task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,35 @@
 #include "main.h"
+
 /**
- * read_textfile - Reads a text file and prints its content to stdout.
- * @filename: The path to the text file to be read.
- * @letters: The maximum number of characters to read and print.
- * Return: The number of characters successfully read and printed, or 0.
-*/
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	ssize_t bytesRead;
-	ssize_t bytesWritten;
-	FILE *file;
+	int fd;
+	ssize_t Num_Rd, Num_Wr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	file = fopen(filename, "r");
-	if (file == NULL)
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
-	buffer = (char *)malloc(letters + 1);
-	if (buffer == NULL)
-	{
-		fclose(file);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
-	bytesRead = fread(buffer, 1, letters, file);
-	if (bytesRead < 0)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	buffer[bytesRead] = '\0';
-	bytesWritten = fwrite(buffer, 1, bytesRead, stdout);
-	if (bytesWritten < bytesRead)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	fclose(file);
-	free(buffer);
-	return (bytesWritten);
+
+	Num_Rd = read(fd, buf, letters);
+	Num_Wr = write(STDOUT_FILENO, buf, Num_Rd);
+
+	close(fd);
+
+	free(buf);
+
+	return (Num_Wr);
 }
