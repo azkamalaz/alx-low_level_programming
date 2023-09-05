125c994d6eba937f26d73de9dbad50cfa0ed104f
azkamalaz <kamalazenag@gmail.com>
2023-09-05 00:24:38 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,39 +7,35 @@
  */
 int main(int argc, char *argv[])
 {
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-	const char *file_from;
-	const char *file_to;
 	int fd_src;
 	int fd_dst;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		return (97);
 	}
-	file_from = argv[1];
-	file_to = argv[2];
-	fd_src = open(file_from, O_RDONLY);
+	fd_src = open(argv[1], O_RDONLY);
 	if (fd_src == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		return (98);
 	}
-	fd_dst = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_dst == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		close(fd_src);
 		return (99);
 	}
-	while ((bytes_read = read(fd_src, buffer, sizeof(buffer))) > 0)
+	while ((bytes_read = read(fd_src, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_dst, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			close(fd_src);
 			close(fd_dst);
 			return (99);
@@ -47,15 +43,12 @@ int main(int argc, char *argv[])
 	}
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_src);
 		close(fd_dst);
 		return (98);
 	}
-	if (close(fd_src) == -1 || close(fd_dst) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		return (100);
-	}
+	close(fd_src);
+	close(fd_dst);
 	return (0);
 }
